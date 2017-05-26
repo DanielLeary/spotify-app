53d734336d672dd371c263bdafde5f17f4073a4e
Daniel Leary <daniel.leary14@imperial.ac.uk>
2017-05-26 01:01:37 +0100

    removed a comment

--- spotify-app-server/app.js
+++ spotify-app-server/app.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
 const api = require('./API');
-/*app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
-  next();
- });*/
+
 app.use('/api', api);
 
 
